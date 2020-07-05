b6f40e5ed6207b551ec779bcc6ef0459a1b9e67f
akshayDatacode <akshaywebllisto@gmsil.com>
2020-07-05 15:47:54 +0530

    Fetch Api Connected with React using Axios getMessages

--- server/server.js
+++ server/server.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
+const cors = require("cors");
 
 const indexRoutes = require("./routes/indexRoute");
 
@@ -10,6 +11,7 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(bodyParser.json());
+app.use(cors());
 
 //using routes
 app.use("/api", indexRoutes);
--- src/components/ListMessageCompoonent.jsx
+++ src/components/ListMessageCompoonent.jsx
@@ -1,30 +1,56 @@
 import React, { Component } from "react";
+import axios from "axios";
+
+const API = "http://www.localhost:5000/api/get_messages";
 
 class ListMessageComponent extends Component {
-  state = {};
+  state = {
+    message: [],
+    isLoading: false,
+    error: null,
+  };
+
+  componentDidMount() {
+    this.setState({ isLoading: true });
+    console.log("Did Mount");
+    axios
+      .get(API)
+      .then((result) =>
+        this.setState({
+          message: result.data.message,
+          isLoading: false,
+        })
+      )
+      .catch((error) =>
+        this.setState({
+          error,
+          isLoading: false,
+        })
+      );
+  }
+
   render() {
     return (
       <>
         <h3 className="text-center mb-3">Messages List </h3>
-
-        <div>
-          <div className="card border-primary shadow">
-            <div className="card-header m-0 pl-3 pb-1 pt-1">
-              Sender : Akshay
-            </div>
-            <div className="card-body m-0 pl-3 pb-0 pt-2">
-              {" "}
-              <p>
-                Body : Hello Akshay How Are you I am Fine What About Your Time
-              </p>{" "}
-            </div>
-            <div className="card-header text-right m-0 pl-4 pb-2 bg-white">
-              <button type="reset" className="btn btn-danger ml-5 p-1">
-                Delete
-              </button>
+        {this.state.message.map((item) => (
+          <div>
+            <div className="card border-primary shadow mb-2">
+              <div className="card-header m-0 pl-3 pb-1 pt-1">
+                Sender : {item.sender}
+              </div>
+              <div className="card-body m-0 pl-3 pb-0 pt-2">
+                {" "}
+                <p>Body : {item.body}</p>{" "}
+              </div>
+              <div className="card-header text-right m-0 pl-4 pb-2 bg-white">
+                <button type="reset" className="btn btn-danger ml-5 p-1">
+                  Delete
+                </button>
+              </div>
             </div>
           </div>
-        </div>
+        ))}
       </>
     );
   }
